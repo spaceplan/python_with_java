b8d6aaef0bd5a48dd81a19c1e25e3ef9a3b00a18
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2020-08-30 16:22:55 +0800

    123

--- src/com/xutongxin/main/Main.java
+++ src/com/xutongxin/main/Main.java
@@ -6,23 +6,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
+import com.xutongxin.methob.Function;
 
 
 public class Main {
+    public static int line = 1, begin = 0, ifword = 0, tab = 0;
 
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         //String fileroad="C://Users//xtx//Documents//a.txt";
-        String fileroad="E://a.txt";
+        String fileroad = "E://a.txt";
         //BufferedReader是可以按行读取文件
         FileInputStream inputStream = new FileInputStream(fileroad);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
         String str = null;
-        while((str = bufferedReader.readLine()) != null)
-        {
+        while ((str = bufferedReader.readLine()) != null) {
             System.out.println(str.charAt(1));
+            line = line + 1;
         }
 
         //close
@@ -30,14 +30,38 @@ public class Main {
         bufferedReader.close();
 
     }
-    public static void run(String str)
-    {
-        switch (str.charAt(0)) {
-            case ' ':
+
+    public static void run(String str) {
+        if (tab!=0&& str.charAt(0)!='\t') {
+            tab = 0;
+            return;
+        }
+        switch (str.charAt(tab)) {
+            case ' ': {
                 System.out.println("你的编程规范呢");
+                return;
+            }
             case 'd':
-                
-        }
-        }
+                if (str.substring(0, 4) == "def ")
+                    Function.setFunction(str.substring(4, str.indexOf("(")), line);
+            case 'i':
+                if ("if __name__ == \"__main__\":" == str.substring(0, 26)) {
+                    begin = line;
+                    tab += 1;
+                } else if ("if" == str.substring(0, 2)) {
+                    ifword += 1;
+                    tab += 1;
+                }
+
+                else if ("elif" == str.substring(0, 4) && ifword != 0)
+
+                else if ("else" == str.substring(0, 4) && ifword != 0)
+            case 'p':
+                if(str.substring(0, 6) == "print(")
+                    System.out.println(str.substring(7,str.indexOf(")") ));
+        //case 'o':
+        //if(str.substring(0,4) == "out.")
+
     }
+}
 }
